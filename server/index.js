f2323cf4338c4ef60e8c97280d93cec54b8a5233
tiagopazhs <tiagopaz.hs@gmail.com>
2023-03-17 09:57:09 -0300

    add server structure

---
+++ server/index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/', (req, res) => {
+  res.json({ message: 'Hello World!' });
+});
+
+app.listen(3001, () => {
+  console.log('Server running on port 3001');
+});
