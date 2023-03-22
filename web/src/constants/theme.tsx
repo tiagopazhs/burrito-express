8254f40dfca608707dd9bf639c9c598aab4d26f0
tiagopazhs <tiagopaz.hs@gmail.com>
2023-03-22 05:49:55 -0300

    style: Remove lineHeigth

--- web/src/constants/theme.tsx
+++ web/src/constants/theme.tsx
@@ -21,6 +21,7 @@ export const SIZES = {
     font: 14,
     radius: 30,
     radiusCategory: 20,
+    paddingA: 5,
     padding: 10,
     padding2: 12,
 
@@ -42,16 +43,16 @@ export const SIZES = {
 };
 
 export const FONTS = {
-    largeTitle: { fontFamily: "Roboto-regular", fontSize: SIZES.largeTitle, lineHeight: 55 },
-    h1: { fontFamily: "Roboto-Black", fontSize: SIZES.h1, lineHeight: 36 },
-    h2: { fontFamily: "Roboto-Bold", fontSize: SIZES.h2, lineHeight: 30 },
-    h3: { fontFamily: "Roboto-Bold", fontSize: SIZES.h3, lineHeight: 22 },
-    h4: { fontFamily: "Roboto-Bold", fontSize: SIZES.h4, lineHeight: 22 },
-    body1: { fontFamily: "Roboto-Regular", fontSize: SIZES.body1, lineHeight: 36 },
-    body2: { fontFamily: "Roboto-Regular", fontSize: SIZES.body2, lineHeight: 30 },
-    body3: { fontFamily: "Roboto-Regular", fontSize: SIZES.body3, lineHeight: 22 },
-    body4: { fontFamily: "Roboto-Regular", fontSize: SIZES.body4, lineHeight: 22 },
-    body5: { fontFamily: "Roboto-Regular", fontSize: SIZES.body5, lineHeight: 22 },
+    largeTitle: { fontFamily: "Roboto-regular", fontSize: SIZES.largeTitle},
+    h1: { fontFamily: "Roboto-Black", fontSize: SIZES.h1},
+    h2: { fontFamily: "Roboto-Bold", fontSize: SIZES.h2},
+    h3: { fontFamily: "Roboto-Bold", fontSize: SIZES.h3},
+    h4: { fontFamily: "Roboto-Bold", fontSize: SIZES.h4},
+    body1: { fontFamily: "Roboto-Regular", fontSize: SIZES.body1},
+    body2: { fontFamily: "Roboto-Regular", fontSize: SIZES.body2},
+    body3: { fontFamily: "Roboto-Regular", fontSize: SIZES.body3},
+    body4: { fontFamily: "Roboto-Regular", fontSize: SIZES.body4},
+    body5: { fontFamily: "Roboto-Regular", fontSize: SIZES.body5},
 };
 
 export interface AppTheme {
