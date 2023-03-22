2c31d22f5264a70251da265c233bae049f31684c
tiagopazhs <tiagopaz.hs@gmail.com>
2023-03-22 05:50:52 -0300

    feat: Crete TopBar align

--- web/src/components/TopBar.tsx
+++ web/src/components/TopBar.tsx
@@ -1,22 +1,23 @@
 
 import { COLORS, SIZES, FONTS } from '../constants';
+import Grid from '@mui/material/Grid'
 
-const buttonStyle = { width: '15vw', padding: SIZES.padding, backgroundColor: 'transparent' }
+const buttonStyle = { height: '100%', width: '15%', padding: SIZES.padding, backgroundColor: 'transparent' }
 
 export default function TopBar() {
     return (
-        <div className="flex justify-center items-center">
-            <button style={buttonStyle}>
-                <img src={process.env.PUBLIC_URL + '/icons/list.png'} alt="nearby" />
-            </button>
-
-            <div className="flex justify-center items-center" style={{ width: '70vw', backgroundColor: COLORS.lightGray3, borderRadius: SIZES.radius }}>
-                <div style={{ ...FONTS.h4 }}>Monterrey, Mexico</div>
-            </div>
-
-            <button style={buttonStyle}>
-                <img src={process.env.PUBLIC_URL + '/icons/shopping-basket.png'} alt="basket" />
-            </button>
-        </div>
+        <Grid container spacing={1} className="flex justify-center items-center" style={{ height: '10vh' }}>
+            <Grid item style={buttonStyle} className="flex justify-center items-center" >
+                <img src={process.env.PUBLIC_URL + '/icons/list.png'} alt="list" style={{ height: '100%', padding: SIZES.paddingA }} />
+            </Grid>
+            <Grid item style={{ height: '100%', width: '70%', padding: SIZES.padding2 }}>
+                <div className="flex justify-center items-center" style={{ height: '100%', backgroundColor: COLORS.lightGray3, borderRadius: SIZES.radius}}>
+                    <div style={{ ...FONTS.h4 }}>Monterrey, Mexico</div>
+                </div>
+            </Grid>
+            <Grid item style={buttonStyle} className="flex justify-center items-center" >
+                <img src={process.env.PUBLIC_URL + '/icons/shopping-basket.png'} alt="basket" style={{ height: '100%', padding: SIZES.paddingA }} />
+            </Grid>
+        </Grid >
     );
 }
