8dc70a04dfbc4680c85f36ee2b1b0472bbc24508
tiagopazhs <tiagopaz.hs@gmail.com>
2023-03-22 21:13:03 -0300

    feat: Create orders list

--- web/src/pages/Home.tsx
+++ web/src/pages/Home.tsx
@@ -2,18 +2,15 @@ import React from 'react';
 import TopBar from '../components/TopBar';
 import CategoryList from '../components/CategoryList';
 import { SIZES } from '../constants';
+import OrderList from '../components/OrderList';
 
 function Home(): JSX.Element {
   return (
-    <>
       <div style={{padding: SIZES.padding2}}>
         <TopBar />
         <CategoryList />
-        <div style={{ height: '80vh' }} >
-          This is Home page
-        </div>
+        <OrderList />
       </div>
-    </>
   );
 }
 
