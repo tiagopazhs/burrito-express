b71c087360c1888ca2696f5bc23dbe9d73d35eb7
tiagopazhs <tiagopaz.hs@gmail.com>
2023-03-25 17:05:42 -0300

    refact: Rewrite topBar without location

--- web/src/components/TopBar.tsx
+++ web/src/components/TopBar.tsx
@@ -1,23 +1,15 @@
 
-import { COLORS, SIZES, FONTS } from '../constants';
-import Grid from '@mui/material/Grid'
-
-const buttonStyle = { height: '100%', width: '15%', padding: SIZES.padding, backgroundColor: 'transparent' }
+import { COLORS } from '../constants';
 
 export default function TopBar() {
-    return (
-        <Grid container spacing={1} className="flex justify-center items-center" style={{ height: '10vh' }}>
-            <Grid item style={buttonStyle} className="flex justify-center items-center" >
-                <img src={process.env.PUBLIC_URL + '/icons/list.png'} alt="list" style={{ height: '100%', padding: SIZES.paddingA }} />
-            </Grid>
-            <Grid item style={{ height: '100%', width: '70%', padding: SIZES.padding2 }}>
-                <div className="flex justify-center items-center" style={{ height: '100%', backgroundColor: COLORS.lightGray3, borderRadius: SIZES.radius}}>
-                    <div style={{ ...FONTS.h4 }}>Monterrey, Mexico</div>
-                </div>
-            </Grid>
-            <Grid item style={buttonStyle} className="flex justify-center items-center" >
-                <img src={process.env.PUBLIC_URL + '/icons/shopping-basket.png'} alt="basket" style={{ height: '100%', padding: SIZES.paddingA }} />
-            </Grid>
-        </Grid >
-    );
+  return (
+    <div className="flex justify-between top-0 left-0 right-0 fixed" style={{ height: '10vh', backgroundColor: COLORS.primary}}>
+      <div className="flex items-center px-4">
+        <img src={process.env.PUBLIC_URL + '/icons/list.png'} alt="list" style={{ height: '55%' }} />
+      </div>
+      <div className="flex items-center px-4">
+        <img src={process.env.PUBLIC_URL + '/icons/shopping-basket.png'} alt="basket" style={{ height: '55%' }} />
+      </div>
+    </div>
+  );
 }
