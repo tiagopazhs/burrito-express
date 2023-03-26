689c2f53fddf6e26bf473d87770caa221ffdfbac
tiagopazhs <tiagopaz.hs@gmail.com>
2023-03-26 18:10:55 -0300

    feat: Create restaurant info

--- web/src/components/BackgroundImage.tsx
+++ web/src/components/BackgroundImage.tsx
@@ -1,7 +1,14 @@
+import { COLORS } from '../constants';
+
 export default function BackgroundImage() {
     return (
         <div style={{ height: '50vh', marginTop: '10vh' }}>
-            <img src={process.env.PUBLIC_URL + 'background/bg6.jpg'} alt="list" style={{ width: '100%' }} className="blur-xm" />
+            <img src={process.env.PUBLIC_URL + 'background/bg6.jpg'} alt="list" style={{ marginBottom: '-25vh', width: '100%' }} />
+            <div className="grid justify-center rounded-md" style={{ color: COLORS.white, backgroundColor: COLORS.black, height: '10vh' }}>
+                <div className="grid justify-center text-4xl font-bold" style={{backgroundColor: COLORS.terciary}} >BURRITO EXPRESS</div>
+                <div className="grid justify-center text-xl" style={{backgroundColor: COLORS.terciary}}>📍Avenida Constitución, 43, Monterrey, México.</div>
+                <div className="grid justify-center text-xl" style={{backgroundColor: COLORS.terciary}}>🕐 65-75 min</div>
+            </div>
         </div>
     )
 }
