7902d652ab72ec9df034b2b41f118a682ef53719
tiagopazhs <tiagopaz.hs@gmail.com>
2023-03-26 17:16:24 -0300

    feat: Update background image

--- web/src/components/BackgroundImage.tsx
+++ web/src/components/BackgroundImage.tsx
@@ -1,7 +1,7 @@
 export default function BackgroundImage() {
     return (
         <div style={{ height: '50vh', marginTop: '10vh' }}>
-            <img src={process.env.PUBLIC_URL + '/bg5.jpeg'} alt="list" style={{ width: '100%' }} className="" />
+            <img src={process.env.PUBLIC_URL + 'background/bg6.jpg'} alt="list" style={{ width: '100%' }} className="blur-xm" />
         </div>
     )
 }
