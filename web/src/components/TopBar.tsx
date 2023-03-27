5a04ab8e21283a1308fbc90a4802b6a41adf3f58
tiagopazhs <tiagopaz.hs@gmail.com>
2023-03-27 07:24:27 -0300

    feat: Add responsivity to the topBar

--- web/src/components/TopBar.tsx
+++ web/src/components/TopBar.tsx
@@ -1,14 +1,13 @@
-
 import { COLORS } from '../constants';
 
 export default function TopBar() {
   return (
-    <div className="flex justify-between top-0 left-0 right-0 fixed" style={{ height: '10vh', backgroundColor: COLORS.primary}}>
+    <div className="h-9 md:h-10 lg:h-12 2xl:h-14 flex justify-between top-0 left-0 right-0 fixed" style={{ backgroundColor: COLORS.primary }}>
       <div className="flex items-center px-4">
-        <img src={process.env.PUBLIC_URL + '/icons/list.png'} alt="list" style={{ height: '55%' }} />
+        <img className="h-5 md:h-6 lg:h-7 2xl:h-9" src={process.env.PUBLIC_URL + '/icons/list.png'} alt="list" />
       </div>
       <div className="flex items-center px-4">
-        <img src={process.env.PUBLIC_URL + '/icons/shopping-basket.png'} alt="basket" style={{ height: '55%' }} />
+        <img className="h-5 md:h-6 lg:h-7 2xl:h-9"src={process.env.PUBLIC_URL + '/icons/shopping-basket.png'} alt="basket"/>
       </div>
     </div>
   );
