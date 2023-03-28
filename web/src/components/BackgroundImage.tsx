e79a79e5251a6cf73bc9e0efafad1e6e3561f3d3
tiagopazhs <tiagopaz.hs@gmail.com>
2023-03-28 11:53:23 -0300

    feat: Add responsivity to image background

--- web/src/components/BackgroundImage.tsx
+++ web/src/components/BackgroundImage.tsx
@@ -2,12 +2,13 @@ import { COLORS } from '../constants';
 
 export default function BackgroundImage() {
     return (
-        <div style={{ height: '50vh', marginTop: '10vh' }}>
-            <img src={process.env.PUBLIC_URL + 'background/bg6.jpg'} alt="list" style={{ marginBottom: '-25vh', width: '100%' }} />
-            <div className="grid justify-center rounded-md" style={{ color: COLORS.white, backgroundColor: COLORS.black, height: '10vh' }}>
-                <div className="grid justify-center text-4xl font-bold" style={{backgroundColor: COLORS.terciary}} >BURRITO EXPRESS</div>
-                <div className="grid justify-center text-xl" style={{backgroundColor: COLORS.terciary}}>📍Avenida Constitución, 43, Monterrey, México.</div>
-                <div className="grid justify-center text-xl" style={{backgroundColor: COLORS.terciary}}>🕐 65-75 min</div>
+        <div className='relative mt-9 md:mt-10 lg:mt-12 2xl:mt-14'>
+            <img className='w-screen h-32 sm:h-36 md:h-40 lg:h-48 xl:h-56 2xl:h-64'
+                src={process.env.PUBLIC_URL + 'background/bg6.jpg'} alt="list" />
+           <div className="absolute text-center grid justify-center top-1/2 left-1/2 -translate-y-1/2 -translate-x-1/2 max-w-screen-sm mx-auto w-48 sm:w-64 md:w-1/2" style={{ color: COLORS.white}}>
+                <div className="text-lg font-bold sm:text-xl md:text-3xl lg:text-5xl xl:text-6xl" style={{ backgroundColor: COLORS.terciary }} >BURRITO EXPRESS</div>
+                <div className="text-xs sm:text-sm" style={{ backgroundColor: COLORS.terciary }}>📍Avenida Constitución, 43, Monterrey, Nuevo León, México.</div>
+                <div className="text-xs sm:text-sm" style={{ backgroundColor: COLORS.terciary }}>🕐 65-75 min</div>
             </div>
         </div>
     )
